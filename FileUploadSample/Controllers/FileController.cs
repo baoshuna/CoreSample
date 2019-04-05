02454ad6d2250aaacab6bf04067347912cdf3427
Zhiwen Lin <1130120850@qq.com>
2019-04-05 12:01:46 +0800

    文件上传 IFormFile

---
+++ FileUploadSample/Controllers/FileController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace FileUploadSample.Controllers
+{
+    public class FileController : Controller
+    {
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            // 文件即将存放的文件夹
+            var fileFolder = Path.Combine(Directory.GetCurrentDirectory(), "Files");
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var filePath = Path.Combine(fileFolder, formFile.FileName);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            // process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok(new { count = files.Count, size, fileFolder });
+        }
+
+    }
+}
