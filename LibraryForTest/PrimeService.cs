50d6ea793c651da3d7f2c907ea8548f00c4ccafa
zhiwen <zhiwen@kooboo.cn>
2019-04-14 01:01:06 +0800

    UT啊UT

---
+++ LibraryForTest/PrimeService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryForTest
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int id)
+        {
+            if (id == 1)
+            {
+                return false;
+            }
+
+            throw new NotImplementedException("Please create a prime first");
+        }
+    }
+}
---
+++ LibraryUseXunit.Test/PrimeServiceTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace LibraryUseXunit.Test
+{
+    public class PrimeServiceTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
